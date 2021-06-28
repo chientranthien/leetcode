7d04b2886902da0c666bddf9b66e372085d99bab
Tran Thien Chien <chien_tran@chien.tran>
2021-06-28 14:38:58 +0800

    Beatify code

--- medium/221.maximal-square.go
+++ medium/221.maximal-square.go
@@ -1,21 +1,20 @@
-
 func maximalSquare(matrix [][]byte) int {
 	m := len(matrix)
 	n := len(matrix[0])
 	max := 0
 	mem := make([][]int, m+1)
 	for i := range mem {
-			mem[i] = make([]int, n+1)
+		mem[i] = make([]int, n+1)
 	}
 
-	for i:=1; i<=m; i++ {
-		for j:=1; j<=n; j++ {
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
 			if matrix[i-1][j-1] != '1' {
 				continue
 			}
 			mem[i][j] = min(mem[i-1][j-1], mem[i-1][j], mem[i][j-1]) + 1
 			w := mem[i][j]
-			area := w*w
+			area := w * w
 			if area > max {
 				max = area
 			}
@@ -23,7 +22,7 @@ func maximalSquare(matrix [][]byte) int {
 	}
 	return max
 }
-func min(vals...int) int {
+func min(vals ...int) int {
 	m := vals[0]
 	for _, val := range vals {
 		if val < m {
